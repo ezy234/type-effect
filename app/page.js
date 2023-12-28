cb932979f8de5b91ddfdc73c5eec1e3edbb03fc2
IsraelIyke <nwangwuisrael@gmail.com>
2023-12-29 00:56:01 +0100

    animate with css

--- app/page.js
+++ app/page.js
@@ -3,19 +3,18 @@ import "animate.css";
 export default function Home() {
   return (
     <div className=" flex overflow-hidden justify-center mt-[10rem]">
-      <div className="back-div"></div>
-      <div className="animate_animated animate__lightSpeedInRight  glass text-center w-[50vw] h-[40vh]">
-        <h1 className="animate_animated animatetada animate_delay-1s text-[5rem] font-[800] text-orange-900">
+      <div className="bg-div"></div>
+      <div className="animate__animated animate__lightSpeedInRight  glass text-center w-[50vw] h-[40vh]">
+        <h1 className="animate__animated animate__tada animate_delay-1s text-[5rem] font-[800] text-orange-900">
           Animate<span className=" text-black">.</span>css
         </h1>
-        <h1 className="animate_animated animatezoomInDown animate_delay-3s text-[1.2rem] font-[500] mt-[0.5rem]">
+        <h1 className="animate__animated animate__zoomInDown animate_delay-3s text-[1.2rem] font-[500] mt-[0.5rem]">
           A cross-browser library for CSS animation
         </h1>
-        <h1 className="animate_animated animatezoomInDown animate_delay-4s text-[1rem] font-[300] mt-[0.5rem]">
+        <h1 className="animate__animated animate__zoomInDown animate_delay-4s text-[1rem] font-[300] mt-[0.5rem]">
           Animate without Javascript
         </h1>
       </div>
-         
     </div>
   );
 }
