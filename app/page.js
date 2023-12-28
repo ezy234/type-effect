7c57c0b6be11c54035e82e980bfd4c6677b2d27d
IsraelIyke <nwangwuisrael@gmail.com>
2023-12-29 00:51:02 +0100

    animate with css

--- app/page.js
+++ app/page.js
@@ -1,96 +1,21 @@
-"use client";
-import Image from "next/image";
-import { useEffect, useRef, useState } from "react";
-import WaveSurfer from "wavesurfer.js";
-import {
-  BsFillStopFill,
-  BsFillPlayFill,
-  BsSkipForward,
-  BsSkipBackward,
-} from "react-icons/bs";
+import "animate.css";
 
 export default function Home() {
-  const waveformRef = useRef(null);
-  let wavesurfer;
-  const [playPause, setPlayPause] = useState();
-
-  useEffect(() => {
-    wavesurfer = WaveSurfer.create({
-      container: waveformRef.current,
-      waveColor: "#34374B",
-      progressColor: "#F90",
-      url: "/oceans.mp3",
-      dragToSeek: true,
-      width: "35vw",
-      hideScrollbar: true,
-      normalize: true,
-      barGap: 1,
-      height: 60,
-      barHeight: 20,
-      barRadius: 20,
-      barWidth: 5,
-    });
-
-    wavesurfer.on("finish", () => {
-      console.log("song finished");
-    });
-
-    wavesurfer.on("ready", () => {
-      console.log("Waveform is ready");
-    });
-    return () => {
-      wavesurfer.destroy();
-    };
-  }, []);
-  const handleStop = () => {
-    if (wavesurfer) {
-      wavesurfer.stop();
-    }
-  };
-  const handlePause = () => {
-    if (wavesurfer) {
-      wavesurfer.playPause();
-    }
-  };
-
-  const handleSkipForward = () => {
-    if (wavesurfer) {
-      wavesurfer.skip(2);
-    }
-  };
-  const handleSkipBack = () => {
-    if (wavesurfer) {
-      wavesurfer.skip(-2);
-    }
-  };
-
   return (
-    <div className="container">
-      <div className="sub-container">
-        <Image
-          src="/oceans.jpg"
-          width={1000}
-          height={1000}
-          className="audio-image"
-        />
-        <p>Oceans</p>
-
-        <div ref={waveformRef} className="wavesurfer-container" />
-        <div className="wavesurfer-controls">
-          <button onClick={handleSkipBack}>
-            <BsSkipBackward />
-          </button>
-          <button onClick={handlePause}>
-            <BsFillPlayFill />
-          </button>
-          <button onClick={handleStop}>
-            <BsFillStopFill />
-          </button>
-          <button onClick={handleSkipForward}>
-            <BsSkipForward />
-          </button>
-        </div>
+    <div className=" flex overflow-hidden justify-center mt-[10rem]">
+      <div className="back-div"></div>
+      <div className="animate_animated animate__lightSpeedInRight  glass text-center w-[50vw] h-[40vh]">
+        <h1 className="animate_animated animatetada animate_delay-1s text-[5rem] font-[800] text-orange-900">
+          Animate<span className=" text-black">.</span>css
+        </h1>
+        <h1 className="animate_animated animatezoomInDown animate_delay-3s text-[1.2rem] font-[500] mt-[0.5rem]">
+          A cross-browser library for CSS animation
+        </h1>
+        <h1 className="animate_animated animatezoomInDown animate_delay-4s text-[1rem] font-[300] mt-[0.5rem]">
+          Animate without Javascript
+        </h1>
       </div>
+         
     </div>
   );
 }
