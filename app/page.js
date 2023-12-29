5554335558192038b8c7a03d387852fd508c8ac9
IsraelIyke <nwangwuisrael@gmail.com>
2023-12-29 02:20:05 +0100

    finishing touches on styling

--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div className=" flex justify-center items-center h-screen text-center overflow-hidden">
       <div className="bg-div"></div>
-      <div className="glass w-[50vw] h-[40vh] animate__animated animate__lightSpeedInRight">
-        <h1 className="animate__animated animate__tada animate__delay-1s text-[5rem] font-[800] text-orange-900">
+      <div className="glass w-[80vw] h-[40vh] md:w-[50vw] md:h-[40vh] animate__animated animate__lightSpeedInRight flex flex-col justify-center">
+        <h1 className="animate__animated animate__tada animate__delay-1s text-[3rem] md:text-[5rem] font-[800] text-orange-900">
           Animate<span className=" text-black">.</span>css
         </h1>
         <p className="animate__animated animate__zoomInDown animate__delay-4s text-[1.2rem] font-[500] mt-[0.5rem]">
