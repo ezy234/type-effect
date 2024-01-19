c269bd507c79ce3adc464a60dc061b4e86b9ccf7
IsraelIyke <nwangwuisrael@gmail.com>
2024-01-19 23:32:41 +0100

    ok

--- app/page.js
+++ app/page.js
@@ -1,3 +1,20 @@
+"use client";
+import { TypeAnimation } from "react-type-animation";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <TypeAnimation
+        sequence={[
+          "I'm a Software Engineer",
+          500,
+          "I'm a Youtuber",
+          600,
+          "I'm a Embedded Systems Engineer ",
+          700,
+        ]}
+        repeat={0}
+      />
+    </div>
+  );
 }
